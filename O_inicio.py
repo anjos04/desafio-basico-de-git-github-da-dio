73b007fe8f74b1ebb64e366fbff22cc20de373be
Sr. Henrique <c.henrique.anjosdasilva@gmail.com>
2022-10-28 22:24:34 -0300

    inicio da jornada

---
+++ O_inicio.py
@@ -0,0 +1,8 @@
+nome = input("qual é o seu nome? ")
+nota = int(input("qual é a sua nota? "))
+
+if nota > 5:
+    print('você está apto para ser um maior programador')
+
+else:
+    print('você falta muito para se tornarr um grande programador')
